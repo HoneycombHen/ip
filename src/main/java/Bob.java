c48d259cf22b15048088ee41dfd1150b0bdf4f2b
Hendrick <hendricklim8685@gmail.com>
2026-08-24 17:08:14 +0800

    Add greeting

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Bob {
     public static void main(String[] args) {
         String banner = " ____        _     \n"
@@ -5,6 +7,15 @@ public class Bob {
                 + "|  _ \\ / _ \\| '_ \\ \n"
                 + "| |_) | (_) | |_) |\n"
                 + "|____/ \\___/|_.__/ \n";
+        String hor_line = "____________________________________________________________";
+        System.out.println(hor_line);
         System.out.println(banner);
+        System.out.println("Hello! I'm Bob.\n" +
+                "What can I do for you?");
+        System.out.println(hor_line);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(hor_line);
+
     }
 }
