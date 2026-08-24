441a152a01288702ed64c71d9115107ce21ae823
Hendrick <hendricklim8685@gmail.com>
2026-08-24 18:01:44 +0800

    Add stored list feature

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -7,26 +7,42 @@ public class Bob {
                 + "|  _ \\ / _ \\| '_ \\ \n"
                 + "| |_) | (_) | |_) |\n"
                 + "|____/ \\___/|_.__/ \n";
-        String hor_line = "____________________________________________________________";
-        System.out.println(hor_line);
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine);
         System.out.println(banner);
         System.out.println("Hello! I'm Bob.\n" +
                 "What can I do for you?");
-        System.out.println(hor_line);
+        System.out.println(horizontalLine);
+
+        String[] taskList = new String[100];
+        int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
-            System.out.println(hor_line);
 
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(hor_line);
+                System.out.println(horizontalLine);
                 break;
             }
 
-            System.out.println(command);
-            System.out.println(hor_line);
+            else if (command.equals("list")) {
+                for (int j = 0; j < taskCount; j++) {
+                    System.out.printf("%d. %s\n", (j + 1), taskList[j]);
+                }
+                System.out.println(horizontalLine);
+            }
+
+            else {
+                if (taskCount < taskList.length) {
+                    taskList[taskCount] = command;
+                    taskCount++;
+                    System.out.println("added: " + command);
+                } else {
+                    System.out.println("Your task list is full.");
+                }
+            }
         }
 
     }
