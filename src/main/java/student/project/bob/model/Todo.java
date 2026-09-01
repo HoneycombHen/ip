3217c02d02f6bb426e75caa87fc1c42a4c4b3399
Hendrick <hendricklim8685@gmail.com>
2026-09-02 03:00:15 +0800

    Add Javadocs to Bob task APIs
    Several public members in Bob's entry point and task model lack complete
    documentation.
    
    Add Javadocs for the application entry point, task constructors, state
    mutators, and task string representations to satisfy the a-javadoc stretch
    goal without changing runtime behavior.

--- src/main/java/student/project/bob/model/Todo.java
+++ src/main/java/student/project/bob/model/Todo.java
@@ -5,10 +5,20 @@ package student.project.bob.model;
  */
 public class Todo extends Task {
 
+    /**
+     * Creates an incomplete todo task.
+     *
+     * @param description description of the todo
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Returns this todo in Bob's display and persistence format.
+     *
+     * @return the todo's status and description
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
